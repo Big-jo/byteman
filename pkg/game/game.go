9e19e0cae2be4b398384320c32f507aa47e16a25
Zaza.codes <jovienloba1@gmail.com>
2025-07-26 00:45:43 +0100

    refactor renderer import and increase map size in game initialization

--- pkg/game/game.go
+++ pkg/game/game.go
@@ -1,7 +1,7 @@
 package game
 
 import (
-	"byteman/pkg/render"
+	renderer "byteman/pkg/render"
 	"byteman/pkg/world"
 	"log"
 
@@ -16,7 +16,7 @@ type GameState struct {
 
 func NewGame() GameState {
 	player := &world.Player{Pos: world.Vec2{X: 1, Y: 1}}
-	worldMap := world.Loadmap(24, 24)
+	worldMap := world.Loadmap(128, 128)
 	gameState := GameState{tiles: worldMap, player: player, isRunning: true}
 
 	return gameState
